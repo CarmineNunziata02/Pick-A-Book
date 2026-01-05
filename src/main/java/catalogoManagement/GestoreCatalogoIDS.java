b43971dbc39de56cf2b4efa2faab8a376f9626c0
Felice Chirico <127207103+f3lice01@users.noreply.github.com>
2026-01-05 23:11:28 +0100

    completamento implementazione dei metodi del GestoreCatalogo

--- src/main/java/catalogoManagement/GestoreCatalogoIDS.java
+++ src/main/java/catalogoManagement/GestoreCatalogoIDS.java
@@ -27,22 +27,98 @@ public class GestoreCatalogoIDS implements GestoreCatalogoDAO{
 
 	@Override
 	public Boolean doSaveGestore(GestoreCatalogo gestoreCatalogo) throws SQLException {
+		String query = "INSERT INTO " + GestoreCatalogoIDS.TABLE
+				+ " (email_address, nome, cognome, password) VALUES (?, ?, ?, ?)";
 
+		try (Connection connection = ds.getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+
+			String hashedPassword = PasswordUtils.hashPassword(gestoreCatalogo.getPassword());
+
+
+			preparedStatement.setString(1, gestoreCatalogo.getEmail());
+			preparedStatement.setString(2, gestoreCatalogo.getNome());
+			preparedStatement.setString(3, gestoreCatalogo.getCognome());
+			preparedStatement.setString(4, hashedPassword);
+
+			if(preparedStatement.executeUpdate()>0)
+				return true;
+		} catch (SQLException e) {
+			logger.log(Level.ALL, ERROR, e);
+		}
+		return false;
 	}
 
 	@Override
 	public Boolean doUpdateGestore(GestoreCatalogo gestoreCatalogo) throws SQLException {
+		String query = "UPDATE " + GestoreCatalogoIDS.TABLE
+				+ "SET nome = ?, cognome = ?, password = ? "
+				+ "WHERE email = ?";
+
+		try (Connection connection = ds.getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+
+			String hashedPassword = PasswordUtils.hashPassword(gestoreCatalogo.getPassword());
+
+			preparedStatement.setString(1, gestoreCatalogo.getNome());
+			preparedStatement.setString(2, gestoreCatalogo.getCognome());
+			preparedStatement.setString(3, hashedPassword);
+			preparedStatement.setString(4, gestoreCatalogo.getEmail());
+
+			if(preparedStatement.executeUpdate()>0)
+				return true;
+		} catch (SQLException e) {
+			logger.log(Level.ALL, ERROR, e);
+		}
+
+		return false;
 
 	}
 
 	@Override
 	public Boolean doDeleteGestore(String email) throws SQLException {
+		String query = "DELETE FROM " + GestoreCatalogoIDS.TABLE + " WHERE email_address = ?";
+
+		try (Connection connection = ds.getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+
+			preparedStatement.setString(1, email);
+
+			if(preparedStatement.executeUpdate()>0)
+				return true;
+		} catch (SQLException e) {
+			logger.log(Level.ALL, ERROR, e);
+		}
+		return false;
 
 	}
 
 	@Override
 	public GestoreCatalogo doRetrieveByAuthentication(String email, String password) throws SQLException {
+		String query = "SELECT * FROM " + GestoreCatalogoIDS.TABLE + " WHERE email_address = ? and password = ?";
 
+		try (Connection connection = ds.getConnection();
+			 PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+			String hashedPassword = PasswordUtils.hashPassword(password);
+
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, hashedPassword);
+
+			ResultSet rs = preparedStatement.executeQuery();
+			if(rs.next()) {
+				String nome = rs.getString(NOME);
+				String cognome = rs.getString(COGNOME);
+
+				return new GestoreCatalogo(email, nome, cognome, hashedPassword);
+			}
+
+			rs.close();
+
+		} catch (SQLException e) {
+			logger.log(Level.ALL, ERROR, e);
+		}
+
+		return null;
 	}
 
 	/*** MACRO ***/
