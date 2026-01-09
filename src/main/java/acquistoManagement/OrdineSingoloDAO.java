eb9ad174d666733ba19fb4b05c7bc98f0fde6f37
Felice Chirico <127207103+f3lice01@users.noreply.github.com>
2026-01-10 00:06:16 +0100

    creazione dell'interfaccia per gli ordini singoli da account guest

---
+++ src/main/java/acquistoManagement/OrdineSingoloDAO.java
@@ -0,0 +1,18 @@
+package acquistoManagement;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface OrdineSingoloDAO {
+	public Boolean doSaveOrdineSingolo(OrdineSingolo ordineSingolo) throws SQLException;
+
+	public Boolean doDeleteOrdineSingolo(Integer id) throws SQLException;
+
+	public Boolean doUpdateOrdineSingolo(OrdineSingolo ordineSingolo) throws SQLException;
+
+	public Collection<OrdineSingolo> doRetrieveAllOrdineSingolo() throws SQLException;
+
+	public Collection<OrdineSingolo> doRetrieveAllByOrdineId(Integer ordineId) throws SQLException;
+	
+	public OrdineSingolo doRetrieveById(Integer id) throws SQLException;
+}
